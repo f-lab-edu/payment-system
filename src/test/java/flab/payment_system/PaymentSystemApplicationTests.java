a50ff63c008381b4f786171f3577d1e20e623315
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2023-06-01 18:54:06 +0900

    feat : init

---
+++ src/test/java/flab/payment_system/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
