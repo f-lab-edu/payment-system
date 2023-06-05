491163f9f3874d0c37b0673dac4cb26966753fb9
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2023-06-05 17:25:55 +0900

    chore : reflecting code style

--- src/main/java/flab/payment_system/PaymentSystemApplication.java
+++ src/main/java/flab/payment_system/PaymentSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSystemApplication.class, args);
+    }
 
 }
