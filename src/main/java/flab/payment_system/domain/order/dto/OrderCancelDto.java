96052d06024c8e675ae89f2af0f7a02b86846f66
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2023-07-07 04:29:28 +0900

    refactor : add validation check for order, payment domain

--- src/main/java/flab/payment_system/domain/order/dto/OrderCancelDto.java
+++ src/main/java/flab/payment_system/domain/order/dto/OrderCancelDto.java
@@ -1,11 +1,19 @@
 package flab.payment_system.domain.order.dto;
 
+import jakarta.validation.constraints.Min;
+
 public record OrderCancelDto(
 	String paymentKey,
+	@Min(value = 1, message = "invalid_cancel_amount")
 	Integer cancelAmount,
+	@Min(value = 1, message = "invalid_cancel_tax_free_amount")
 	Integer cancelTaxFreeAmount,
-	Integer orderId,
-	Integer productId
+	@Min(value = 1, message = "invalid_order_id")
+	long orderId,
+	@Min(value = 1, message = "invalid_product_id")
+	long productId,
+	@Min(value = 1, message = "invalid_quantity")
+	Integer quantity
 ) {
 
 }
--- src/main/java/flab/payment_system/domain/order/dto/OrderProductDto.java
+++ src/main/java/flab/payment_system/domain/order/dto/OrderProductDto.java
@@ -1,14 +1,19 @@
 package flab.payment_system.domain.order.dto;
 
+import jakarta.validation.constraints.Min;
 import java.util.Optional;
 
 public record OrderProductDto(
 	String productName,
+	@Min(value = 1, message = "invalid_product_id")
 	long productId,
+	@Min(value = 1, message = "invalid_quantity")
 	int quantity,
-	int totalAmount,
-	int taxFreeAmount,
-	int installMonth
+	@Min(value = 1, message = "invalid_total_amount")
+	Integer totalAmount,
+	@Min(value = 1, message = "invalid_tax_free_amount")
+	Integer taxFreeAmount,
+	Integer installMonth
 ) {
 
 	public Optional<Integer> getInstallMonth() {
