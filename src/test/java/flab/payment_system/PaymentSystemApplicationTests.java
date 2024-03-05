0e6310a0a7022fbb958b637e5823c6711b0e28a3
Zoe <98700133+Zoe0631@users.noreply.github.com>
2024-03-05 20:51:26 +0900

    test : use application-test.yml
    use application-test.yml

--- src/test/java/flab/payment_system/PaymentSystemApplicationTests.java
+++ src/test/java/flab/payment_system/PaymentSystemApplicationTests.java
@@ -3,7 +3,7 @@ package flab.payment_system;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.config.location=classpath:application-test.yml"})
 class PaymentSystemApplicationTests {
 
 	@Test
